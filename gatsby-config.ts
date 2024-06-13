2e0b8b1a6edf073d9cba860b775669e8064b176c
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-06-13 13:59:37 +0100

    .

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -13,7 +13,7 @@ module.exports = {
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://tobyhogan.space`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
