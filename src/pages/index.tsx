6d7d34c73964aa594c9d5ed2024b1bd49da6a7e7
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-06-13 14:20:43 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ const widgetsRowStyles = {
 
 const projectWidgetStyles = {
   border: "2px solid black",
-  borderRadius: "16px",
+  borderRadius: "8px",
   width: "28%",
   margin: "1%",
   textAlign: "center",
@@ -32,14 +32,15 @@ const IndexPage: React.FC<PageProps> = () => {
     <main className="px-64">
       <h1 className="mt-5 mb-8 text-center italic">tobyhogan.space</h1>
       <div className="px-8 mx-auto w-fit">
-        <p className="w-64 text-center">Hey I'm Toby, a UK based indie dev; making stuff some people find useful.</p>
+        <p className="w-64 text-center">Hey I'm Toby, a UK based indie dev; making some stuff people hopefully find useful.</p>
         <p className="text-center my-5">Some of my projects: </p>
       </div>
-      <div className="mx-auto flex border-2 border-black rounded-lg w-fit">
+      <div className="mx-auto flex border-2 border-black rounded-md w-fit">
         <div style={widgetsRowStyles}>
           <div style={projectWidgetStyles}>
             <a href="https://kaizenprojects.co" className="underline text-blue-500" target="_blank">Habitazen</a>
             <img src={habtiazen_screenshot}></img>
+            <p className="mb-3">A progressive habit tracker.</p>
           </div>
           <div style={projectWidgetStyles}>
             <p>To added...</p>
