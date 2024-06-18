f1986b24b7c60049c9f728ab9dc6be548d26fd22
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-06-18 05:36:05 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,8 @@ const widgetsRowStyles = {
 }
 
 const projectWidgetStyles = {
-  border: "2px solid black",
+  backgroundColor: "#f5f5f5",
+  border: "2px solid gray",
   borderRadius: "8px",
   width: "28%",
   margin: "1%",
@@ -23,35 +24,38 @@ const projectWidgetStyles = {
   display: "flex",
   flexDirection: "column",
 
+
 }
 
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main className="px-64">
-      <h1 className="mt-5 mb-8 text-center italic">tobyhogan.space</h1>
+      <h1 className="mt-5 mb-8 text-center italic text-xl">tobyhogan.space</h1>
       <div className="px-8 mx-auto w-fit">
-        <p className="w-64 text-center">Hey I'm Toby, a UK based indie dev; making some stuff people hopefully find useful.</p>
-        <p className="text-center my-5">Some of my projects: </p>
+        <h6 className="text-center text-3xl font-bold mb-3">Indie Dev: Toby</h6>
+        <p className="w-64 text-center text-md">Hey I'm Toby, a UK based software devleoper; making some stuff people hopefully find useful :)</p>
+        <p className="text-center my-5 text-lg">Some of my projects: </p>
       </div>
-      <div className="mx-auto flex border-2 border-black rounded-md w-fit">
+      <div className="mx-auto flex rounded-md w-fit">
         <div style={widgetsRowStyles}>
-          <div style={projectWidgetStyles}>
-            <a href="https://kaizenprojects.co" className="underline text-slate-500 my-2" target="_blank">Habitazen</a>
-            <img src={habtiazen_screenshot}></img>
-            <p className="mb-3">A progressive habit tracker.</p>
+          <div style={projectWidgetStyles} className="ProjectWidget">
+            <a href="https://kaizenprojects.co" target="_blank">
+              <img src={habtiazen_screenshot}></img>
+              <p className="mb-3">A progressive habit tracker.</p>
+            </a>
           </div>
-          <div style={projectWidgetStyles}>
+          <div style={projectWidgetStyles} className="ProjectWidget">
             <p>To be added...</p>
           </div>
-          <div style={projectWidgetStyles}>
+          <div style={projectWidgetStyles} className="ProjectWidget">
             <p>To be added...</p>
           </div>
         </div>
         <div></div>
         <div></div>
       </div>
-      <p className="text-center mt-12">tobyhogan.space 2024 ©</p>
+      <p className="text-center mt-12 font-bold">tobyhogan.space © 2024</p>
     </main>
   )
 }
