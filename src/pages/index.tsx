f5e3aff1ddf137110e2ed4ff0e1b7ad93f69d18c
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-06-19 17:17:55 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,6 +40,7 @@ const IndexPage: React.FC<PageProps> = () => {
         <div style={widgetsRowStyles} className="WidgetsRow">
           <div style={projectWidgetStyles} className="ProjectWidget">
             <a href="https://kaizenprojects.co" target="_blank">
+              <h1 className="">Habitazen</h1>
               <img src={habtiazen_screenshot}></img>
               <p className="mb-3">A flexible habit tracker.</p>
             </a>
