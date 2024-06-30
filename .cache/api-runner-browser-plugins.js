42fdfc66b18f5421a437001a73880fc8eeb10672
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-06-30 15:05:10 +0100

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/.pnpm/gatsby-plugin-manifest@5.13.1_gatsby@5.13.6_babel-eslint@10.1.0_eslint@7.32.0__react-dom@18.3_jlbmpt4hr46nhk5wd2yeymkmua/node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"icon":"src/assets/icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"fbbdb40488fd9905461d5a44dd88e664"},
-    },{
-      plugin: require('../node_modules/.pnpm/gatsby@5.13.6_babel-eslint@10.1.0_eslint@7.32.0__react-dom@18.3.1_react@18.3.1__react@18.3.1__mq6ksqwrbnjt5mkthjz6fiytki/node_modules/gatsby/dist/internal-plugins/partytown/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
