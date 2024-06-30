874e6dce9931c6be54ed2feef7edd167b5259c4b
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-06-30 13:42:51 +0100

    .

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---src-pages-404-tsx": preferDefault(require("C:\\Users\\tobyh\\Desktop\\coding-projects\\tobyhogan-space\\src\\pages\\404.tsx")),
+  "component---src-pages-index-tsx": preferDefault(require("C:\\Users\\tobyh\\Desktop\\coding-projects\\tobyhogan-space\\src\\pages\\index.tsx"))
+}
+
---
+++ .cache/page-ssr/routes/component---src-pages-index-tsx.js
@@ -0,0 +1,108 @@
+"use strict";
+exports.id = 245;
+exports.ids = [245];
+exports.modules = {
+
+/***/ 1238:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  Head: () => (/* binding */ Head),
+  "default": () => (/* binding */ pages)
+});
+
+// EXTERNAL MODULE: external "C:\\Users\\tobyh\\Desktop\\coding-projects\\tobyhogan-space\\node_modules\\.pnpm\\react@18.3.1\\node_modules\\react\\index.js"
+var external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_ = __webpack_require__(821);
+;// CONCATENATED MODULE: ./src/assets/habitazen.png
+/* harmony default export */ const habitazen = ("/static/habitazen-ba6fd94a16482b69da1dcf08ecd84dec.png");
+// EXTERNAL MODULE: ./node_modules/.pnpm/react-icons@5.2.1_react@18.3.1/node_modules/react-icons/vsc/index.mjs
+var vsc = __webpack_require__(8339);
+// EXTERNAL MODULE: ./node_modules/.pnpm/react-icons@5.2.1_react@18.3.1/node_modules/react-icons/ai/index.mjs
+var ai = __webpack_require__(9177);
+;// CONCATENATED MODULE: ./src/pages/index.tsx
+
+
+
+
+
+
+const widgetsRowStyles = {
+  margin: "auto",
+  justifyContent: "space-between"
+};
+const projectWidgetStyles = {
+  backgroundColor: "#f1e9e9",
+  border: "2px solid gray",
+  borderRadius: "8px",
+  width: "28%",
+  margin: "1%",
+  textAlign: "center",
+  display: "flex",
+  flexDirection: "column"
+};
+const IndexPage = () => {
+  return /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("main", {
+    className: ""
+  }, /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("div", {
+    className: "flex flex-row mx-auto w-fit mb-8"
+  }, /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("h1", {
+    className: "mt-5 text-center text-neutral-500 text-lg tracking-wider ml-16"
+  }, "tobyhogan.space"), /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("a", {
+    href: "https://github.com/tobyhogan",
+    target: "_blank"
+  }, /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement(vsc/* VscGithub */.ioy, {
+    size: 30,
+    className: "mt-4 ml-3 text-grayNew-550 hover:text-grayNew-650"
+  })), /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("a", {
+    href: "https://open.spotify.com/user/kjejvfmuzwmjmkdykulvb39hm",
+    target: "_blank"
+  }, /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement(ai/* AiOutlineSpotify */.hdp, {
+    size: 33,
+    className: "mt-3.5 ml-2 text-grayNew-550 hover:text-grayNew-650"
+  }))), /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("div", {
+    className: "px-8 mx-auto w-fit"
+  }, /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("h6", {
+    className: "text-center text-3xl font-bold mb-3"
+  }, "Toby: Indie Dev"), /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("p", {
+    className: "w-64 text-center text-md"
+  }, "Hey I'm Toby, a UK based software devleoper; making some stuff people hopefully find useful :)"), /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("p", {
+    className: "text-center my-5 text-lg"
+  }, "Some of my projects: ")), /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("div", {
+    className: "mx-auto rounded-md w-fit"
+  }, /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("div", {
+    style: widgetsRowStyles,
+    className: "WidgetsRow"
+  }, /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("div", {
+    style: projectWidgetStyles,
+    className: "ProjectWidget"
+  }, /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("a", {
+    href: "https://kaizenprojects.co",
+    target: "_blank"
+  }, /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("h1", {
+    className: ""
+  }, "Habitazen"), /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("img", {
+    src: habitazen
+  }), /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("p", {
+    className: "mb-3"
+  }, "A flexible habit tracker."))), /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("div", {
+    style: projectWidgetStyles,
+    className: "ProjectWidget"
+  }, /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("p", null, "To be added...")), /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("div", {
+    style: projectWidgetStyles,
+    className: "ProjectWidget"
+  }, /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("p", null, "To be added..."))), /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("div", null), /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("div", null)), /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("p", {
+    className: "text-center mt-12 font-bold"
+  }, "\xA9 tobyhogan.space 2024"));
+};
+/* harmony default export */ const pages = (IndexPage);
+const Head = () => /*#__PURE__*/external_C_Users_tobyh_Desktop_coding_projects_tobyhogan_space_node_modules_pnpm_react_18_3_1_node_modules_react_index_js_.createElement("title", null, "Home Page");
+
+/***/ })
+
+};
+;
+//# sourceMappingURL=component---src-pages-index-tsx.js.map
