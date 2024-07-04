7cc3cd6f2f6d1873efe7aad5f26074e0be7aac37
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-04 21:50:40 +0100

    .

--- src/pages/blog/{markdownRemark.frontmatter__slug}.jsx
+++ src/pages/blog/{markdownRemark.frontmatter__slug}.jsx
@@ -8,10 +8,10 @@ export default function BlogPostTemplate({
   const { frontmatter, html } = markdownRemark
   return (
     <div className="mt-6">
-      <a href="/" className="ml-12 border-2 rounded-md border-black px-3 py-0.5 bg-grayNew-300">Back</a>
+      <a href="/blog" className="ml-[3vw] border-2 rounded-md border-black px-3 py-0.5 bg-grayNew-300 fixed">Back</a>
       <h1 className="text-2xl mt-6 text-center">{frontmatter.title}</h1>
       <h2 className="font-bold text-center mt-3">{frontmatter.date}</h2>
-      <div dangerouslySetInnerHTML={{ __html: html }} className="px-[35rem] mt-12 mb-20 text-[17px] [&>h2]:text-xl [&>h2]:underline [&>h2]:-ml-3 whitespace-break-spaces"/>
+      <div dangerouslySetInnerHTML={{ __html: html }} className="md:px-[30vw] sm:px-[12vw] mt-12 mb-20 text-[17px] [&>h2]:text-xl [&>h2]:underline [&>h2]:-ml-3 whitespace-break-spaces"/>
     </div>
   )
 }
