32ad76b476d30f030f73a155b82deacbc026a6c2
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-05 09:07:25 +0100

    .

--- src/pages/blog/{markdownRemark.frontmatter__slug}.jsx
+++ src/pages/blog/{markdownRemark.frontmatter__slug}.jsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 
+import '../../styles/global.css'
+import '../../styles/index.css'
 
 import Footer from '../../components/footer'
 
@@ -11,8 +13,8 @@ export default function BlogPostTemplate({
   const { frontmatter, html } = markdownRemark
   return (
     <div className="mt-6">
-      <a href="/blog" className="ml-[3vw] -mt-6 border-2 rounded-md border-black px-3 py-0.5 bg-grayNew-300 md:fixed sm:static">Back</a>
-      <h1 className="text-2xl mt-14 text-center">{frontmatter.title}</h1>
+      <a href="/blog" className="ml-[3vw] -mt-8 border-2 rounded-md border-black px-3 py-0.5 bg-grayNew-300 md:fixed sm:static">Back</a>
+      <h1 className="text-2xl md:mt-16 sm:mt-4 text-center">{frontmatter.title}</h1>
       <h2 className="font-bold text-center mt-3">{frontmatter.date}</h2>
       <div dangerouslySetInnerHTML={{ __html: html }} className="md:px-[30vw] sm:px-[12vw] mt-12 text-[17px] [&>h2]:text-xl [&>h2]:underline [&>h2]:-ml-3 whitespace-break-spaces"/>
       <Footer />
