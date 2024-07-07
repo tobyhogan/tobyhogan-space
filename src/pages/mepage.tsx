0fccb7aa4e04ccfd93d7417ea89b1139ebd9c2d8
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-07 18:38:20 +0100

    .

--- src/pages/mepage.tsx
+++ src/pages/mepage.tsx
@@ -16,17 +16,21 @@ const MePage: React.FC<PageProps> = () => {
   return (
     <main className="">
       <Header />
-      <h1>Interests</h1>
-      <ul>
-        <li>Music</li>
-          <h2>Projects:</h2>
-            <ul>
-              <li>Browser Music Viewer: </li>
-            </ul>
-        <li>Photo</li>
-        <li>Art</li>
-        <li></li>
-      </ul>
+      <section className="w-fit mx-auto">
+        <h1 className="text-2xl text-center mt-10">Interests</h1>
+        <ul className="mt-8 ml-8">
+          <li>
+            <h2 className="text-lg">Music</h2>
+            <h3>- Projects:</h3>
+            <a href="https://browser-music-viewer-9s0daum6l-toby-hogans-projects.vercel.app/" className="ml-4">- <u>Browser Song Stem Mixer</u></a>
+          </li>
+
+          <li>Photo</li>
+          <li>Art</li>
+          <li></li>
+        </ul>
+      </section>
+
       
       
       <Footer />
