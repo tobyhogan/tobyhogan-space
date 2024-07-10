b5c2258fe4d1788ff64b903c2f5ef633e275c4d2
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-10 21:10:26 +0100

    .

--- src/pages/blog/{markdownRemark.frontmatter__slug}.jsx
+++ src/pages/blog/{markdownRemark.frontmatter__slug}.jsx
@@ -15,7 +15,7 @@ export default function BlogPostTemplate({
     <div className="mt-6">
       <a href="/blog" className="ml-[3vw] -mt-8 rounded-md px-4 py-1 bg-grayNew-300 md:fixed sm:static border-2 border-grayNew-700 font-semibold">Back</a>
       <h1 className="text-2xl md:mt-16 sm:mt-4 text-center">{frontmatter.title}</h1>
-      <h2 className="font-bold text-center mt-3">{frontmatter.date}</h2>
+      <h4 className="font-bold text-center mt-3">{frontmatter.date}</h4>
       <div dangerouslySetInnerHTML={{ __html: html }} className="md:px-[30vw] sm:px-[12vw] mt-12 text-[17px] [&>h2]:text-xl [&>h2]:underline [&>h2]:-ml-3 whitespace-break-spaces"/>
       <Footer />
     </div>
