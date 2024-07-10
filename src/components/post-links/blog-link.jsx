79d17270df7a2cd4d90e40d14985df64812c2ba2
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-10 14:33:35 +0100

    .

--- src/components/post-links/blog-link.jsx
+++ src/components/post-links/blog-link.jsx
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 const PostLink = ({ post }) => (
   <div>
-    <Link to={'' + post.frontmatter.slug} className="flex">•<p className="underline ml-2 mr-1">{post.frontmatter.title}</p>(@ {post.frontmatter.date})</Link>
+    <Link to={'' + post.frontmatter.slug} className="flex ml-4">•<p className="underline ml-2 mr-1">{post.frontmatter.title}</p>(@ {post.frontmatter.date})</Link>
   </div>
 )
 
