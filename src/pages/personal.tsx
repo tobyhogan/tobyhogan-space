e31858644a2c6f003b11fd168094f914c323e2c2
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-10 19:37:38 +0100

    .

--- src/pages/personal.tsx
+++ src/pages/personal.tsx
@@ -34,7 +34,7 @@ const PersonalPage: React.FC<PageProps> = () => {
         </ul>
 
         <div className="mt-20 text-center">
-          <a href="/admin-page" className="">⊗</a>
+          <a href="/admin-page">⊗</a>
         </div>
 
       </section>
