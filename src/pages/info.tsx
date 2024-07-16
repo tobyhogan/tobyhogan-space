982971106df9e74c08d1140a8121ea81ccb5e7e2
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-16 18:56:10 +0100

    .

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -18,9 +18,10 @@ const InfoPage: React.FC<PageProps> = () => {
         <h3 className="text-center mt-4">Contact:</h3>
         <div className="ml-8 text-start flex flex-col">
           <a href="mailto:tobyhogan44@gmail.com" className="mt-4">- Email: <u>tobyhogan44@gmail.com</u></a>
-          <a href="https://github.com/tobyhogan">- GitHub: @<u>tobyhogan</u></a>
+          <a href="https://github.com/tobyhogan" target="_blank">- GitHub: @<u>tobyhogan</u></a>
+          <a href="https://stackoverflow.com/users/14429974/toby-hogan" target="_blank">- Stack Overflow: @<u>tobyhogan</u></a>
           <p>- Discord: @dev_tobyh</p>
-          <a href="https://www.indiehackers.com/Dev_TobyH">- IndieHackers: @<u>Dev_TobyH</u></a>
+          <a href="https://www.indiehackers.com/Dev_TobyH" target="_blank">- IndieHackers: @<u>Dev_TobyH</u></a>
         </div>
       </div>
       <Footer />
