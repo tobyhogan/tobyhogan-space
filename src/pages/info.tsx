e36ae820632f7d216a7880470db842516080feb4
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-16 19:21:51 +0100

    .

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -17,7 +17,7 @@ const InfoPage: React.FC<PageProps> = () => {
       <Header />
       <div className="px-8 mx-auto w-fit">
         <h1 className="text-center text-3xl font-bold mb-3">Information</h1>
-        <h3 className="text-center mt-4">Contact:</h3>
+        <h3 className="text-center mt-4">Contact & Profiles:</h3>
         <div className="ml-8 text-start flex flex-col">
           <p className="mt-4">- Email: tobyhogan44@gmail.com; <a href="mailto:tobyhogan44@gmail.com"><u>Link</u></a></p>
           <p>- Discord: @dev_tobyh</p>
