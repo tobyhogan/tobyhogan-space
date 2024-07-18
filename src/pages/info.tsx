03cdd40148858bbfcaa71152a48ec6292370b7ca
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-18 17:18:31 +0100

    .

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -18,7 +18,7 @@ const InfoPage: React.FC<PageProps> = () => {
       <div className="mx-auto w-fit">
         <h1 className="text-center text-3xl font-bold mb-3">Information</h1>
         <h3 className="text-center mt-4">Contact & Profiles:</h3>
-        <div className="px-[8%] mt-2 text-start flex flex-col">
+        <div className="InfoLinks ml-14 mt-2 text-start flex flex-col">
           <p className="mt-4">- <b>Email:</b> tobyhogan44@gmail.com</p>
           <p>- <b>Discord: </b>@dev_tobyh</p>
           <p className="LContainer1">-&nbsp;<b>IndieHackers:&nbsp;</b><a href="https://www.indiehackers.com/Dev_TobyH" target="_blank" className="Link1">@<u>Dev_TobyH</u></a></p>
