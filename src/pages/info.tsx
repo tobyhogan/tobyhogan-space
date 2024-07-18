9dea0c72fa50296eef0de61a33a5147ba412d76a
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-18 17:11:19 +0100

    .

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -15,16 +15,16 @@ const InfoPage: React.FC<PageProps> = () => {
   return (
     <main className="">
       <Header />
-      <div className="px-8 mx-auto w-fit">
+      <div className="mx-auto w-fit">
         <h1 className="text-center text-3xl font-bold mb-3">Information</h1>
         <h3 className="text-center mt-4">Contact & Profiles:</h3>
-        <div className="ml-10 mt-2 text-start flex flex-col">
+        <div className="px-[8%] mt-2 text-start flex flex-col">
           <p className="mt-4">- <b>Email:</b> tobyhogan44@gmail.com</p>
           <p>- <b>Discord: </b>@dev_tobyh</p>
-          <a href="https://www.indiehackers.com/Dev_TobyH" target="_blank" className="Link1">-&nbsp;<b>IndieHackers:&nbsp;</b><p>@<u>Dev_TobyH</u></p></a>
-          <a href="https://github.com/tobyhogan" target="_blank" className="Link1">-&nbsp;<b>GitHub:&nbsp;</b><p>@<u>tobyhogan</u></p></a>
-          <a href="https://mastodon.social/@tobyhogan" target="_blank" className="Link1">-&nbsp;<b>Mastodon:&nbsp;</b><p>@<u>tobyhogan</u></p></a>
-          <a href="https://stackoverflow.com/users/14429974/toby-hogan" target="_blank" className="Link1">-&nbsp;<b>Stack Overflow:&nbsp;</b><p><u>Toby Hogan; user:14429974</u></p></a>
+          <p className="LContainer1">-&nbsp;<b>IndieHackers:&nbsp;</b><a href="https://www.indiehackers.com/Dev_TobyH" target="_blank" className="Link1">@<u>Dev_TobyH</u></a></p>
+          <p className="LContainer1">-&nbsp;<b>GitHub:&nbsp;</b><a href="https://github.com/tobyhogan" target="_blank" className="Link1">@<u>tobyhogan</u></a></p>
+          <p className="LContainer1">-&nbsp;<b>Mastodon:&nbsp;</b><a href="https://mastodon.social/@tobyhogan" target="_blank" className="Link1">@<u>tobyhogan</u></a></p>
+          <p className="LContainer1">-<b>&nbsp;Stack&nbsp;Overflow:&thinsp;</b><a href="https://stackoverflow.com/users/14429974/toby-hogan" target="_blank" className="Link1"><u>Toby&nbsp;Hogan;&nbsp;user:14429974</u></a></p>
         </div>
       </div>
       <Footer />
