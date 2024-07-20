fc6613914b65414c88a331fb0cf7a28e9f147005
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-20 15:39:06 +0100

    .

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -20,11 +20,11 @@ const InfoPage: React.FC<PageProps> = () => {
         <h3 className="text-center mt-4">Contact & Profiles:</h3>
         <div className="InfoLinks ml-14 mt-2 text-start flex flex-col">
           <p className="mt-4">- <b>Email:</b> tobyhogan44@gmail.com</p>
-          <p>- <b>Discord: </b>@dev_tobyh</p>
+          <p>- <b>Discord: </b>@tobyh_dev</p>
           <p className="LContainer1">-&nbsp;<b>IndieHackers:&nbsp;</b><a href="https://www.indiehackers.com/Dev_TobyH" target="_blank" className="Link1">@<u>Dev_TobyH</u></a></p>
           <p className="LContainer1">-&nbsp;<b>GitHub:&nbsp;</b><a href="https://github.com/tobyhogan" target="_blank" className="Link1">@<u>tobyhogan</u></a></p>
           <p className="LContainer1">-&nbsp;<b>Mastodon:&nbsp;</b><a href="https://mastodon.social/@tobyhogan" target="_blank" className="Link1">@<u>tobyhogan</u></a></p>
-          <p className="LContainer1">-<b>&nbsp;Stack&nbsp;Overflow:&thinsp;</b><a href="https://stackoverflow.com/users/14429974/toby-hogan" target="_blank" className="Link1"><u>Toby&nbsp;Hogan;&nbsp;user:14429974</u></a></p>
+          <p className="LContainer1">-<b>&nbsp;Stack&nbsp;Overflow:&thinsp;</b><a href="https://stackoverflow.com/users/14429974/toby-hogan" target="_blank" className="Link1"><u>user:14429974</u></a></p>
         </div>
       </div>
       <Footer />
